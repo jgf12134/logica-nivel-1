ba2767bfeb052e04fe64fa8992b3acfc541cb37e
jgf12134 <jorge.franklin.c.s@gmail.com>
2025-04-08 21:57:05 -0300

    ex-01.py

---
+++ ex-01.py
@@ -0,0 +1,28 @@
+"""
+6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+Cadastro de Clientes
+0 - Fim
+1 - Inclui
+2 - Altera
+3 - Exclui
+4 - Consulta
+
+Opção: 
+
+"""
+print ("0-fim ")
+print("1-inclui")
+print("2-altera")
+print("3-excluir")
+print("4-consultar")
+print("opção")
+print("o primeiro programa a gente nunca esquece!")
+print("jorge")
+print("rio de janeiro")
+print("niterói")
+print("proibido letras do justin bieber")
+print("xxxxx")
+print("x   x")
+print("x   x")
+print("x   x")
+print("xxxxx")
