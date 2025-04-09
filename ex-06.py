516648ce894b8bc5160b14a2bcf7c1289e0887ad
jgf12134 <jorge.franklin.c.s@gmail.com>
2025-04-08 21:22:48 -0300

    Create ex-06.py

---
+++ ex-06.py
@@ -0,0 +1,18 @@
+"""
+6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+Cadastro de Clientes
+0 - Fim
+1 - Inclui
+2 - Altera
+3 - Exclui
+4 - Consulta
+
+Opção: 
+
+"""
+print ("0-fim ")
+print("1-inclui")
+print("2-altera")
+print("3-excluir")
+print("4-consultar")
+print("opção")
